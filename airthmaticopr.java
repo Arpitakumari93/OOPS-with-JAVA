8ec7622025918949d3026ef2f519a0ccbd87a890
Arpitakumari93 <24150CN017@glbitm.ac.in>
2025-03-27 16:09:14 +0530

    Add files via upload

---
+++ airthmaticopr.java
@@ -0,0 +1,16 @@
+public class airthmaticopr {
+    public static void main(String[]args)
+    {
+        int a=60,b=30,sum,sub,div,mod;
+        sum=a+b;
+        System.out.println("the sum is :"+ sum);
+        sub=a-b;
+        System.out.println("the sub is:"+ sub);
+        div=a/b;
+        System.out.println("the div is:"+ div);
+        mod=a%b;
+        System.out.println("the mod is:"+ mod);
+
+    }
+    
+}
